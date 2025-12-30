b198b8d8af8216ce708a7757962e9c275028fdd4
Thien Tran <thienforever2211@gmail.com>
2025-12-30 20:12:27 +0700

    add item and category page

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False).order_by('-created_at')[:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {'items': items, 'categories': categories})
 
 
 def contact(request):
---
+++ marketplace/item/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='items')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='items')
+
+    def __str__(self):
+        return self.name
